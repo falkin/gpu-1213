7c35dd40b5053f85b4afbddf4bd0d1bdccba5da7
studentmse7 <nordesk.tasveri@gmail.com>
2012-11-02 19:19:45 +0100

    useJulia implementation

--- OpenMP_Image/src/cpp/main/mainCore.cpp
+++ OpenMP_Image/src/cpp/main/mainCore.cpp
@@ -4,6 +4,7 @@
 #include "ImageViewers.h"
 #include "RipplingMOO.h"
 #include "02_Mandelbrot.h"
+#include "03_Julia.h"
 using std::cout;
 using std::endl;
 
@@ -28,6 +29,7 @@ int mainCore(void);
 
 static bool useRippling(void);
 static bool useMandelbrot(void);
+static bool useJulia(void);
 
 /*----------------------------------------------------------------------*\
  |*         Implementation                  *|
@@ -46,7 +48,8 @@ int mainCore(void)
     //isOk &= useHello();
 
     //isOk &= useRippling ();
-    isOk &= useMandelbrot();
+    //isOk &= useMandelbrot();
+    isOk &= useJulia();
 
     cout << "\n-------------------------------------------------" << endl;
     cout << "End Main : isOk = " << isOk << endl;
@@ -72,15 +75,28 @@ bool useRippling(void)
 bool useMandelbrot(void)
     {
     float x0 = -2.1;
-    float y0 = 1.3;
-    float dx = 2.9;
-    float dy = 2.6;
-    Mandelbrot mandel(512, 512, DomaineMaths(x0, y0, dx, dy));
+    float x1 = 0.8;
+    float y0 = -1.3;
+    float y1 = 1.3;
+
+    Mandelbrot mandel(800, 600, DomaineMaths(x0, y0, x1, y1),12);
     ImageViewers viewer(&mandel);
     ImageViewers::runALL();
     return true;
     }
 
+bool useJulia(void)
+    {
+    float x0 = -1.3;
+    float x1 = 1.3;
+    float y0 = -1.4;
+    float y1 = 1.4;
+
+    Julia julia(800, 600, DomaineMaths(x0, y0, x1, y1),52,-0.12,0.85);
+    ImageViewers viewer(&julia);
+    ImageViewers::runALL();
+    return true;
+    }
 /*----------------------------------------------------------------------*\
  |*         End                     *|
  \*---------------------------------------------------------------------*/
