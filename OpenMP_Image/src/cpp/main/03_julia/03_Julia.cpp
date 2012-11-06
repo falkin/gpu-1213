5fc2818b323806fa0d8e3bd0a7748feb4f125d69
studentmse7 <nordesk.tasveri@gmail.com>
2012-11-06 23:02:25 +0100

    Fix setHue for pixel color

--- OpenMP_Image/src/cpp/main/03_julia/03_Julia.cpp
+++ OpenMP_Image/src/cpp/main/03_julia/03_Julia.cpp
@@ -19,33 +19,6 @@ void Julia::fillImage(const DomaineMaths& domaine)
 	}
     }
 
-/*void Julia::setPixel ( const Point i, const Point j, const DomaineMaths& domaine ) {
- double distX = domaine.x0 + domaine.dx();
- double distY = domaine.y0 + domaine.dy();
- double posX = distX / ( getW () * 1. );
- double posY = distY / ( getH () * 1. );
- double x = 0.;
- double y = 0.;
- double norme = 0.;
-
- int itr = 0;
- // et que pas assez itr
- for ( itr = 0; ( itr < n && norme > 4 ); itr++ ) {
- // tant que ||Z|| < 2
- // calculer x et y ...
- // calculer norme
- x = ( x * x + y * y ) + i;
- y = ( 2 * x * y ) + j;
- norme = x * x + y * y;
- }
- if ( itr == n ) {
- setFloatRGBA ( i, j, 0, 0, 0, 1. );
- } else {
- setHue ( i, j, 1. / itr );
- }
- }
- */
-
 void Julia::setPixel(const Point i, const Point j, const DomaineMaths& domaine)
     {
 
@@ -72,6 +45,6 @@ void Julia::setPixel(const Point i, const Point j, const DomaineMaths& domaine)
 	}
     else
 	{
-	setHue(i, j, 1./(itr-1));
+	setHue(i, j, ((float)(itr-1))/n);
 	}
     }
